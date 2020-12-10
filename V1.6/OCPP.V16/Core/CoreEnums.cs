217bbb648849a56e57aad55701dc0cc7f2e32376
Vincent Baaij <vnbaaij@outlook.com>
2020-12-10 15:32:13 +0100

    remove unused usings

--- V1.6/OCPP.V16/Core/CoreEnums.cs
+++ V1.6/OCPP.V16/Core/CoreEnums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OCPP.V16.Core
+﻿namespace OCPP.V16.Core
 {
     public enum AuthorizationStatus
     {
@@ -21,7 +15,7 @@ namespace OCPP.V16.Core
         Rejected,
         Scheduled,
     }
-    
+
     public enum AvailabilityType
     {
         Inoperative,
--- V1.6/OCPP.V16/ListManagement/ListManagementEnums.cs
+++ V1.6/OCPP.V16/ListManagement/ListManagementEnums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OCPP.V16.ListManagement
+﻿namespace OCPP.V16.ListManagement
 {
     public enum UpdateStatus
     {
