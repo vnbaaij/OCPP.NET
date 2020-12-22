17725e932ad92056a556b5cc631883a3fb84bc64
Vincent Baaij <vnbaaij@outlook.com>
2020-12-22 21:13:52 +0100

    WIP - Replace TestChargePoint console app with Blazor Server App

---
+++ V1.6/BlazorChargePointTest/Components/LogViewer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+
+namespace BlazorChargePointTest.Components
+{
+    public partial class LogViewer : ComponentBase, IDisposable
+    {
+
+        [Parameter]
+        public string Message { get; set; }
+
+
+        [Parameter]
+        public ComponentBase Parent { get; set; }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+
+            int k = -1;
+
+            //builder.OpenElement(k++, "div");
+            //builder.AddAttribute(k++, "id", "listitem" + Guid.NewGuid().ToString("d").Substring(1, 4));
+            //builder.AddAttribute(k++, "style", "width:900px;max-height:26px;position:relative;margin:5px");
+            builder.OpenElement(k++, "span");
+
+
+
+
+
+
+                builder.AddAttribute(k++, "style", "position:relative;cursor:pointer;color:blue");
+
+            //else
+            //{
+            //    builder.AddAttribute(k++, "style", "position:absolute;top:0px;cursor:pointer;color:green");
+            //}
+
+
+
+
+            builder.AddContent(k++, (MarkupString)(Message.Replace("\n","<br />") + "<br />"));
+
+
+
+            builder.CloseElement();
+
+            //builder.CloseElement();
+
+
+
+            base.BuildRenderTree(builder);
+        }
+    }
+}
---
+++ V1.6/BlazorChargePointTest/NotifierService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorChargePointTest
+{
+    public class NotifierService
+    {
+        // Can be called from anywhere
+        public async Task Update()
+        {
+            if (Notify != null)
+            {
+                await Notify.Invoke();
+            }
+        }
+
+        public event Func<Task> Notify;
+    }
+}
